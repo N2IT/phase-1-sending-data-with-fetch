5afaeb1547cb7045cdb7ffc033aeb66f26190861
Tony Eder <tonyeder11@gmail.com>
2023-10-26 14:50:07 -0700

    passed

--- index.js
+++ index.js
@@ -20,8 +20,24 @@ function submitData(name, email){
             email: `${email}`,
         })
     })
-    .then(res => res.json)
-    .then(data => console.log(data))
+    .then(res => res.json())
+    .then(data => appendToDom(data.id))
+    .catch(function (error) {
+        // console.log(error.message);
+        let body = document.querySelector('body')
+        let h3 = document.createElement('h3')
+        h3.textContent = error.message
+        body.appendChild(h3)
+    });
 }
 
-submitData("Tommy Tornado", "spinemup@anyhow.com")
+submitData("Xavi E", "daman@here.com")
+
+
+//Part 2 - HANDLE THE RESPONSE
+function appendToDom(id){
+    let body = document.querySelector('body')
+    let p = document.createElement('p')
+    p.textContent = id
+    body.appendChild(p)
+}
